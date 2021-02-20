bb08cc42946abd61026a800affef4b51305a87ff
Sami Triki <contact@triki.io>
2021-02-20 19:44:37 +0000

    Support autoplay interaction rule. Tweak decibel staticfile and getmicro

--- controller.js
+++ controller.js
@@ -1,16 +1,26 @@
 var mode = 'oscillo';
 
-decibel.staticFile();
 
 var start = function () {
-    decibel.analyser.getByteFrequencyData(decibel.frequencyData);
+    decibel.staticFile();
+    requestAnimationFrame(draw)
+};
 
+function draw () {
+    decibel.analyser.getByteFrequencyData(decibel.frequencyData);
     if (ui[mode].FFT && decibel.analyser.fftSize !== ui[mode].FFT) {
         decibel.setFFT( ui[mode].FFT );
     }
     ui[mode](decibel.analyser, decibel.frequencyData);
-    requestAnimationFrame(start);
-};
+    requestAnimationFrame(draw);
+}
 
-start();
+// Chrome wants a user interaction before starting to play sound, fair enough
+document
+  .getElementById("overlay")
+  .addEventListener("click", function () {
+    document.getElementById("overlay").remove();
+    start();
+  });
+
 
